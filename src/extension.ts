1711cd33fac81f666dd86d58ddf580ff688501c1
Wilhelm Klopp <wil.klopp@gmail.com>
2021-08-19 16:40:25 +0100

    Add sqlite

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,21 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import sqlite3 from "sqlite3";
+import { open as openDatabase, Database } from "sqlite";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "actions-vscode-xvfb-issue" is now active!');
+
+	await openDatabase({
+		filename: "test.sqlite",
+		driver: sqlite3.Database,
+	  });
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
